2fd142ce6dc87abb823f562b63ff09693936a824
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-07 12:11:08 -0300

    fix: Enum status

--- Models/TarefaModel.cs
+++ Models/TarefaModel.cs
@@ -1,10 +1,12 @@
-﻿namespace API_Tarefas.Models
+﻿using API_Tarefas.Enums;
+
+namespace API_Tarefas.Models
 {
     public class TarefaModel
     {
         public int Id { get; set; }
         public string? Nome { get; set; }
         public string Descricao { get; set; }
-        public int Status { get; set; }
+        public StatusTarefa Status { get; set; }
     }
 }
