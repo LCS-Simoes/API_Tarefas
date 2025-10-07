af35f2614fdbb55582f7fa7f343e7fcbc3ac27bf
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-07 12:11:37 -0300

    feat: enum

---
+++ Enums/StatusTarefa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace API_Tarefas.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A fazer")]
+        Afazer = 1,
+        [Description("Em Andamento")]
+        EmAndamento = 2,
+        [Description("Concluido")]
+        Concluída =3
+    }
+}
