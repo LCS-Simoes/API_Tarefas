e38a6b719e3d7cc4e72fbfee7b3e2d90cb3d395b
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-20 17:34:49 -0300

    feat: Controller GET cep

---
+++ Controllers/CepController.cs
@@ -0,0 +1,32 @@
+﻿using API_Tarefas.Integração.Interfaces;
+using API_Tarefas.Integração.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Tarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        private readonly IViaCepIntegracao _viaCepIntegracao;
+        public CepController(IViaCepIntegracao integracao) 
+        {
+            _viaCepIntegracao = integracao;
+        }
+
+
+        [HttpGet("{cep}")]
+        public async Task<ActionResult<ViaCepResponse>> ListarEndereco(string cep)
+        {
+            var response = await _viaCepIntegracao.ObterCep(cep);
+
+            if(response == null)
+            {
+                return BadRequest("CEP Não encontrado!");
+            }
+
+            return Ok(response);
+        }
+    }
+}
